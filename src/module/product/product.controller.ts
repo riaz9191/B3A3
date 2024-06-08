75f51cb87b483aba109be7df26540eaaa8856f23
riaz9191 <riazahammed9191@gmail.com>
2024-06-08 12:34:08 +0600

    product searching by term  successfully

--- src/module/product/product.controller.ts
+++ src/module/product/product.controller.ts
@@ -23,12 +23,22 @@ const createProduct = async (req: Request, res: Response) => {
 
 const getAllProducts = async (req: Request, res: Response) => {
   try {
-    const result = await ProductServices.getAllProducts();
-    res.status(200).json({
-      success: true,
-      message: 'Products fetched successfully!',
-      data: result,
-    });
+    const { searchTerm } = req.query;
+    if (searchTerm) {
+      const result = await ProductServices.searchProducts(searchTerm as string);
+      return res.status(200).json({
+        success: true,
+        message: `Products matching search term '${searchTerm}' fetched successfully!`,
+        data: result,
+      });
+    } else {
+      const result = await ProductServices.getAllProducts();
+      return res.status(200).json({
+        success: true,
+        message: 'Products fetched successfully!',
+        data: result,
+      });
+    }
   } catch (err: any) {
     res.status(500).json({
       success: true,
--- src/module/product/product.service.ts
+++ src/module/product/product.service.ts
@@ -1,17 +1,20 @@
 import { TProduct } from './product.interface';
 import { Product } from './product.model';
 
+//for creating product
 const createProduct = async (payload: TProduct) => {
   const result = new Product(payload);
   await result.save();
   return result;
 };
 
+//for getting all product
 const getAllProducts = async () => {
   const result = await Product.find();
   return result;
 };
 
+//for getting product by id
 const getProductById = async (productId: string) => {
   try {
     const result = await Product.findById(productId);
@@ -24,22 +27,33 @@ const getProductById = async (productId: string) => {
     throw error;
   }
 };
-
+//for updating product by id
 const updateProduct = async (productId: string, payload: Partial<TProduct>) => {
   const result = await Product.findByIdAndUpdate(productId, payload, {
     new: true,
   });
   return result;
 };
+//for deleting product by id
 
 const deleteProduct = async (productId: string) => {
   await Product.findByIdAndDelete(productId);
 };
+//for searching product by term
+
+const searchProducts = async (searchTerm: string) => {
+  const regex = new RegExp(searchTerm, 'i'); // 'i' makes text case-insensitive
+  const result = await Product.find({
+    $or: [{ tags: { $regex: regex } }],
+  });
+  return result
+};
+
 export const ProductServices = {
   createProduct,
   getAllProducts,
   getProductById,
   updateProduct,
   deleteProduct,
-  // searchProducts,
+  searchProducts,
 };
